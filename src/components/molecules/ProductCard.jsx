067ac0f9eb484695281c7cae937856fce89f9175
Alejandro-Murillo20 <jamer153@outlook.com>
2026-04-22 21:24:06 -0500

    septimo commit

--- src/components/molecules/ProductCard.jsx
+++ src/components/molecules/ProductCard.jsx
@@ -7,7 +7,16 @@ const ProductCard = ({ product }) => {
 
   return (
     <div style={{ border: '1px solid #ddd', padding: '10px', borderRadius: '8px', margin: '10px', width: '200px' }}>
-      <img src={product.image} alt={product.title} style={{ width: '100%' }} />
+      <img 
+  src={product.image} 
+  alt={product.title} 
+  style={{ 
+    width: '100%', 
+    height: '150px', // Altura fija para que todas se vean iguales
+    objectFit: 'cover', // Recorta la imagen para que rellene el cuadro sin deformarse
+    borderRadius: '4px' 
+  }} 
+/>
       <h3>{product.title}</h3>
       <p>{product.description}</p>
       <p><strong>${product.price}</strong></p>
